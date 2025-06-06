90937fde045f8a9c0725b44ac333eb47938cfe59
duykhang-bit <67537095+duykhang-bit@users.noreply.github.com>
2025-06-06 19:29:33 +0700

    Configure GitHub Pages deployment

--- src/test/java/test/TestEmail.java
+++ src/test/java/test/TestEmail.java
@@ -4,7 +4,7 @@ import utils.ReportNotifier;
 
 public class TestEmail {
     public static void main(String[] args) {
-        // Test gửi email
-        ReportNotifier.sendReportNotification("leduykhang185@gmail.com");
+        // Hiển thị link report
+        ReportNotifier.showReportLink();
     }
 } 
