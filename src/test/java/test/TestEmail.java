55060e8b9e56b998d22e9376f6a77ab245e1bb4e
duykhang-bit <67537095+duykhang-bit@users.noreply.github.com>
2025-06-06 17:53:42 +0700

    Add GitHub Pages deployment for test reports

---
+++ src/test/java/test/TestEmail.java
@@ -0,0 +1,10 @@
+package test;
+
+import utils.ReportNotifier;
+
+public class TestEmail {
+    public static void main(String[] args) {
+        // Test gửi email
+        ReportNotifier.sendReportNotification("leduykhang185@gmail.com");
+    }
+} 
